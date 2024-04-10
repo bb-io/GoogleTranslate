cca6da9dc62f2f01979e0c63ed7e60c9fc94fa8d
bZverok <137277669+bZverok@users.noreply.github.com>
2024-04-10 12:49:12 +0300

    added categories

--- Apps.GoogleTranslate/GoogleTranslateApplication.cs
+++ Apps.GoogleTranslate/GoogleTranslateApplication.cs
@@ -1,9 +1,17 @@
 ﻿using Blackbird.Applications.Sdk.Common;
+using Blackbird.Applications.Sdk.Common.Metadata;
 
 namespace Apps.GoogleTranslate
 {
-    public class GoogleTranslateApplication : IApplication
+    public class GoogleTranslateApplication : IApplication, ICategoryProvider
     {
+        
+        public IEnumerable<ApplicationCategory> Categories
+        {
+            get => [ApplicationCategory.GoogleApps, ApplicationCategory.MachineTranslationAndMtqe];
+            set { }
+        }
+        
         public string Name
         {
             get => "Google Translate";
