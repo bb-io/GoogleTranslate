7df0dd47a61a46cff76c6aded1f11d4172533964
Alex Terehov <terales@users.noreply.github.com>
2025-09-04 12:08:42 -0230

    Change order of inputs

--- Apps.GoogleTranslate/Models/Requests/BaseTranslationConfig.cs
+++ Apps.GoogleTranslate/Models/Requests/BaseTranslationConfig.cs
@@ -9,6 +9,13 @@ public class BaseTranslationConfig
     [Display("Source language"), DataSource(typeof(LanguageDataHandler))]
     public string? SourceLanguage { get; set; }
 
+    [Display("Glossary name", Description = "Specify if you want to use glossary for translation")]
+    [DataSource(typeof(GlossaryDataHandler))]
+    public string? GlossaryName { get; set; }
+
+    [Display("Ignore case in glossary?", Description = "Specify if you want to ignore keys in glossary, default is true")]
+    public bool? IgnoreGlossaryCase { get; set; }
+
     [Display("Adaptive dataset name", Description = "Specify if you want to translate using dataset")]
     [DataSource(typeof(AdaptiveDatasetDataHandler))]
     public string? AdaptiveDatasetName { get; set; }
@@ -16,11 +23,4 @@ public class BaseTranslationConfig
     [Display("Custom AutoML model", Description = "Specify if you want to translate using custom AutoML model")]
     [DataSource(typeof(AdaptiveDatasetDataHandler))]
     public string? CustomModelName { get; set; }
-
-    [Display("Glossary name", Description = "Specify if you want to use glossary for translation")]
-    [DataSource(typeof(GlossaryDataHandler))]
-    public string? GlossaryName { get; set; }
-
-    [Display("Ignore case in glossary?", Description = "Specify if you want to ignore keys in glossary, default is true")]
-    public bool? IgnoreGlossaryCase { get; set; }
 }
