6a5bdc0f9d759f1d5f35843e49c3ed2323bbe445
vitaliy-bezugly <157373241+vitalii-bezuhlyi@users.noreply.github.com>
2025-09-12 16:50:15 +0300

    fix: AdaptiveDatasetDataHandler

--- Apps.GoogleTranslate/DataSourceHandlers/AdaptiveDatasetDataHandler.cs
+++ Apps.GoogleTranslate/DataSourceHandlers/AdaptiveDatasetDataHandler.cs
@@ -1,5 +1,6 @@
 ﻿using Blackbird.Applications.Sdk.Common.Dynamic;
 using Blackbird.Applications.Sdk.Common.Invocation;
+using Google.Api.Gax.ResourceNames;
 using Google.Cloud.Translate.V3;
 
 namespace Apps.GoogleTranslate.DataSourceHandlers;
@@ -10,9 +11,11 @@ public class AdaptiveDatasetDataHandler(InvocationContext invocationContext)
     public async Task<IEnumerable<DataSourceItem>> GetDataAsync(DataSourceContext context,
         CancellationToken cancellationToken)
     {
+        var projectId = Client.LocationName.ProjectId;
+        var location = new LocationName(projectId, "us-central1");
         var request = new ListAdaptiveMtDatasetsRequest
         {
-            Parent = Client.LocationName.ToString().Replace("/global", "/us-central1"),
+            ParentAsLocationName = location
         };
 
         var adaptiveDatasets = Client.TranslateClient.ListAdaptiveMtDatasetsAsync(request);
